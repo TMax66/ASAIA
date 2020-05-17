ecb84380de8b8cd3e9eba833339a268f7213966e
TMax66 <massimo.tranquillo@gmail.com>
2020-05-18 00:10:10 +0200

    update

--- ZINB2.R
+++ ZINB2.R
@@ -15,22 +15,21 @@
     library(ggeffects)
     library(sjmisc)
     
-    nleish<-read_excel("nparinmacro.xlsx", sheet = "Foglio3")
+    nleish<-read_excel("nparinmacro2.xlsx")
 
-nleish<-nleish %>% 
-mutate("group"=gruppo) %>%
-  select(group,n.leish)
+leish<-nleish %>% 
+  select(group,nleish)
 
 
-nleish<-nleish %>% 
-  mutate(group=factor(group, levels=c("Leishmania","AsaiapHM4","AsaiaWSP","Anfotericina")))
+leish<-leish %>% 
+  mutate(group=factor(group, levels=c("Leishmania","AsaiapHM4","AsaiaWSP")))
 
 #nleish %>% 
 #ggplot(aes(n.leish))+geom_rug(aes(x = n.leish, y = 0), position = position_jitter(height = 0))+
 #facet_wrap(~gruppo)+labs(x="Number of Leishmania in Macrophages", y="N.of Macrophages")+geom_histogram(bins=100)
 
 
-mod <- zeroinfl(n.leish~ group,dist = "negbin",data = nleish)
+mod <- zeroinfl(nleish~ group,dist = "negbin",data = leish)
 
 
 
@@ -39,12 +38,12 @@ mod <- zeroinfl(n.leish~ group,dist = "negbin",data = nleish)
 # # pchisq(2 * (logLik(mod) - logLik(m0)), df = 3, lower.tail=FALSE)
 # 
 p<-plot_model(mod,grid = FALSE,vline.color = "blue", show.intercept = TRUE,
-            show.values = TRUE, show.p = FALSE,transform = NULL)
+             show.values = TRUE, show.p = FALSE,transform = NULL)
 # 
 # 
-# p$conditional+theme_sjplot()+labs(title="Count model")
+#p$conditional+theme_sjplot()+labs(title="Count model")
 # 
-# #p$zero_inflated+theme_sjplot()+labs(title="Zero-inflated model")
+#p$zero_inflated+theme_sjplot()+labs(title="Zero-inflated model")
 # 
 
 
@@ -59,8 +58,8 @@ count<-p[["conditional"]][["data"]]
 
 
 g1<-count %>% 
-  mutate(param = factor(c("Leishmania(L)","AsaiapHM4","AsaiaWSP","L+Amphotericin B"), 
-                        levels=c("L+Amphotericin B","AsaiaWSP","AsaiapHM4","Leishmania(L)"))) %>% 
+  mutate(param = factor(c("Leishmania(L)","AsaiapHM4","AsaiaWSP"), 
+                        levels=c("AsaiaWSP","AsaiapHM4","Leishmania(L)"))) %>% 
   ggplot(aes(x = estimate, y=param, label=p.label))+
   geom_point(aes(color=group), size=2.8)+ scale_color_manual(values=c("blue", "red"))+
   geom_segment(aes(x = conf.low, xend = conf.high, y=param, yend=param,
@@ -72,13 +71,13 @@ g1<-count %>%
 zero<-p[["zero_inflated"]][["data"]]
 
 g2<-zero %>% 
-  mutate(param = factor(c("Leishmania(L)","AsaiapHM4","AsaiaWSP","L+Amphotericin B"), 
-                        levels=c("L+Amphotericin B","AsaiaWSP","AsaiapHM4","Leishmania(L)"))) %>%
-  mutate(xx= c("yes", "no","no","no")) %>% 
+  mutate(param = factor(c("Leishmania(L)","AsaiapHM4","AsaiaWSP"), 
+                        levels=c("AsaiaWSP","AsaiapHM4","Leishmania(L)"))) %>% 
+  mutate(xx= c("yes", "no","no")) %>% 
   ggplot(aes(x = estimate, y=param,label=p.label))+
-  geom_point(aes(color=xx), size=2.8)+ scale_color_manual(values=c("blue", "red"))+
+  geom_point( aes(color=group), size=2.8)+ scale_color_manual(values=c("red", "blue"))+
   geom_segment(aes(x = conf.low, xend = conf.high, y=param, yend=param,
-                   color=xx))+theme_sjplot()+theme(legend.position = "none")+scale_x_continuous( limits=c(-5, 5))+
+                   color=group))+theme_sjplot()+theme(legend.position = "none")+scale_x_continuous( limits=c(-5, 5))+
   vline_0(color="grey3")+labs(title="Zero-inflated Model", x="log-Odds", y="")+
   geom_text(vjust=-1)
 
@@ -89,5 +88,5 @@ g2<-zero %>%
 
 
 tab_model(mod,transform =NULL , string.ci = "95%CI", 
-          file="zinb.html", show.loglik=TRUE, 
+          file="zinb2.html", show.loglik=TRUE, 
           show.aic = TRUE, show.r2 = FALSE)
